3490de6b106e363e08855debd2f5c0330704782e
Anders Nylund <anders@andersnylund.com>
2021-08-29 19:41:36 +0300

    Remove TODO comments and unecessary code

--- src/background/background.ts
+++ src/background/background.ts
@@ -7,10 +7,8 @@ interface TabWithId extends Tabs.Tab {
 
 const tabHasId = (tab: Tabs.Tab): tab is TabWithId => tab.id !== undefined;
 
-// TODO: is this even required?
 tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (tab.url?.includes('clockify.me/tracker')) {
-    // TODO: throttle sending the message
     tabs.sendMessage(tabId, { type: MessageType.REFETCH_BALANCE });
   }
 });
